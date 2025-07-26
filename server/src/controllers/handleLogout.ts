c357730ea6e740f59456f7fa9e35c2138bb55fcf
Prashant <prashantrathore1435@gmail.com>
2025-07-26 18:46:23 +0530

    logout handler ready

--- server/src/controllers/handleLogout.ts
+++ server/src/controllers/handleLogout.ts
@@ -1,3 +1,14 @@
 import { Request, Response } from "express";
 
-export default async function handleLogin(){}
+export default async function handleLogin(req: Request, res: Response) {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    path: "/",
+  });
+  res.status(200).json({
+    success: true,
+    message: "logged out",
+  });
+}
