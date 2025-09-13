038ae1b090f71ba694084b4cb824cc262fe8fd82
Prashant <prashantrathore1435@gmail.com>
2025-09-13 17:47:28 +0530

    fixing cookie issue in next

--- client/lib/getData.ts
+++ client/lib/getData.ts
@@ -1,6 +1,6 @@
 import { ResponseExt } from "@/types/responseExt";
 import axios from "axios";
-import { headers } from "next/headers";
+import { cookies } from "next/headers";
 import { env } from "./env";
 import { redirect } from "next/navigation";
 
@@ -10,12 +10,21 @@ export default async function getData<T>(
   message = "Something went wrong"
 ): Promise<ResponseExt<T>> {
   try {
-    const cookieHeader = (await headers()).get("cookie") ?? "";
+    const cookieStore = await cookies();
+    const cookieHeader = cookieStore.toString();
 
+    console.log("Cookie header:", cookieHeader);
+    console.log("All cookies:", cookieStore.getAll());
+    console.log("Specific auth cookie:", cookieStore.get("token"));
+
+    if (!cookieHeader) {
+      console.log("No cookies found, redirecting to login");
+      redirect("/login?message=No authentication found");
+    }
     const response: ResponseExt<T> = await axios.get(
       `${env.API_URL}${endpoint}`,
       {
-        headers: { Cookie: cookieHeader },
+        headers: { Cookie: cookieHeader, "Content-Type": "application/json" },
       }
     );
     return response;
