0c2ca2d98182e02887a858204d00874486b898a7
Prashant <prashantrathore1435@gmail.com>
2025-09-14 22:17:30 +0530

    redirecting google cb to proxy 2

--- client/app/api/auth/google-cb/route.ts
+++ client/app/api/auth/google-cb/route.ts
@@ -14,15 +14,31 @@ export async function GET(req: NextRequest) {
     const rawCookies = res.headers.get("set-cookie");
     if (rawCookies) responseHeaders.set("set-cookie", rawCookies);
 
-    let redirectUrl = "/login?message=fail 1"; // default fallback
+    let redirectUrl = "/login?message=fail 1";
     if (res.status >= 300 && res.status < 400 && res.headers.get("location")) {
       const location = res.headers.get("location")!;
-      const pathname = new URL(location, env.API_URL).pathname;
-      redirectUrl = location.replace(env.API_URL!, "") || location;
-      //   redirectUrl = pathname.startsWith("/u/")
-      //     ? pathname
-      //     : (redirectUrl = location.replace(env.API_URL!, ""));
+
+      try {
+        const url = new URL(location);
+        const pathWithQuery = url.pathname + url.search;
+
+        if (url.pathname.startsWith("/u/")) {
+          redirectUrl = url.pathname; // Just path for success
+        } else {
+          redirectUrl = pathWithQuery; // Path + query for errors
+        }
+      } catch {
+        redirectUrl = "/login?message=parsing error";
+      }
     }
+    // let redirectUrl = "/login?message=fail 1"; // default fallback
+    // if (res.status >= 300 && res.status < 400 && res.headers.get("location")) {
+    //   const location = res.headers.get("location")!;
+    //   const pathname = new URL(location, env.API_URL).pathname;
+    //   redirectUrl = pathname.startsWith("/u/")
+    //     ? pathname
+    //     : (redirectUrl = location.replace(env.API_URL!, ""));
+    // }
 
     return NextResponse.redirect(redirectUrl, { headers: responseHeaders });
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
