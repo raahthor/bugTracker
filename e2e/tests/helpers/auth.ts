a122303b3a0f66f25b67a49deb1b9b078c40a9e2
Prashant Rathore <prashantrathore1435@gmail.com>
2026-09-26 02:02:23 +0530

    WIP; fixing the playwirght error

--- e2e/tests/helpers/auth.ts
+++ e2e/tests/helpers/auth.ts
@@ -4,6 +4,20 @@ export const username = process.env.TEST_USERNAME!;
 export const password = process.env.TEST_PASSWORD!;
 
 export async function loginAsTestUser(page: Page) {
+  console.log("username exists:", !!username);
+  console.log("NODE_ENV:", process.env.NODE_ENV);
+
+  page.on("response", async (response) => {
+    if (response.request().method() === "POST") {
+      console.log("POST:", response.status(), response.url());
+    }
+  });
+
+  await page.getByRole("button", { name: "Login", exact: true }).click();
+
+  console.log("cookies:", await page.context().cookies());
+  console.log("url:", page.url());
+
   await page.goto("/");
   await page.getByRole("button", { name: "Login", exact: true }).click();
   await expect(page).toHaveURL("/login");
